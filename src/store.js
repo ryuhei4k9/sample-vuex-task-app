a5a1e96f6e9440a5b415f854e55c37bbf2100b25
ryuhei4k9 <ryuhei.4k9@gmail.com>
2019-08-10 12:32:23 +0900

    タスクリスト

--- src/store.js
+++ src/store.js
@@ -4,7 +4,21 @@ import Vuex from "vuex";
 Vue.use(Vuex);
 
 export default new Vuex.Store({
-  state: {},
+  state: {
+    // タスクの初期ステート
+    tasks: [
+      {
+        id: 1,
+        name: "牛乳を買う",
+        done: false
+      },
+      {
+        id: 2,
+        name: "Vue.jsの本を買う",
+        done: true
+      }
+    ]
+  },
   mutations: {},
   actions: {}
 });
