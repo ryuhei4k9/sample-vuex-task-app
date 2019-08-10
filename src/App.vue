7f05c2b17092d57f45f28d17830b6316da6c304d
ryuhei4k9 <ryuhei.4k9@gmail.com>
2019-08-10 13:31:04 +0900

    ラベル表示機能追加

--- src/App.vue
+++ src/App.vue
@@ -9,11 +9,31 @@
           v-on:change="toggleTaskStatus(task)"
         />
         {{ task.name }}
+        -
+        <span v-for="id in task.labelIds" v-bind:key="id">
+          {{ getLabelText(id) }}
+        </span>
       </li>
     </ul>
 
     <form v-on:submit.prevent="addTask">
       <input type="text" v-model="newTaskName" placeholder="新しいタスク" />
+    </form>
+
+    <h2>ラベル一覧</h2>
+    <ul>
+      <li v-for="label in labels" v-bind:key="label.id">
+        <input
+          type="checkbox"
+          v-bind:value="label.id"
+          v-model="newTaskLabelIds"
+        />
+        {{ label.text }}
+      </li>
+    </ul>
+
+    <form v-on:submit.prevent="addTask">
+      <input type="text" v-model="newLabelText" placeholder="新しいラベル" />
     </form>
   </div>
 </template>
@@ -24,13 +44,21 @@ export default {
 
   data() {
     return {
-      newTaskName: ""
+      newTaskName: "",
+
+      newTaskLabelIds: [],
+
+      newLabelText: ""
     };
   },
 
   computed: {
     tasks() {
       return this.$store.state.tasks;
+    },
+
+    labels() {
+      return this.$store.state.labels;
     }
   },
 
@@ -38,9 +66,11 @@ export default {
     addTask() {
       // ストアのaddTaskミューテーションをコミットする
       this.$store.commit("addTask", {
-        name: this.newTaskName
+        name: this.newTaskName,
+        labelIds: this.newTaskLabelIds
       });
       this.newTaskName = "";
+      this.newTaskLabelIds = [];
     },
 
     toggleTaskStatus(task) {
@@ -48,6 +78,19 @@ export default {
       this.$store.commit("toggleTaskStatus", {
         id: task.id
       });
+    },
+
+    addLabel() {
+      this.$store.commit("addLabel", {
+        text: this.newLabelText
+      });
+      this.newLabelText = "";
+    },
+
+    // ラベルIDから、そのラベルのテキストを返す
+    getLabelText(id) {
+      const label = this.labels.filter(label => label.id === id)[0];
+      return label ? label.text : "";
     }
   }
 };
--- src/store.js
+++ src/store.js
@@ -10,23 +10,43 @@ export default new Vuex.Store({
       {
         id: 1,
         name: "牛乳を買う",
+        labelIds: [1, 2],
         done: false
       },
       {
         id: 2,
         name: "Vue.jsの本を買う",
+        labelIds: [1, 3],
         done: true
       }
     ],
 
-    nextTaskId: 3
+    // ラベルの初期ステート
+    labels: [
+      {
+        id: 1,
+        text: "買い物"
+      },
+      {
+        id: 2,
+        text: "食糧"
+      },
+      {
+        id: 3,
+        text: "本"
+      }
+    ],
+
+    nextTaskId: 3,
+    nextLabelId: 4
   },
 
   mutations: {
-    addTask(state, { name }) {
+    addTask(state, { name, labelIds }) {
       state.tasks.push({
         id: state.nextTaskId,
-        name: name,
+        name,
+        labelIds,
         done: false
       });
 
@@ -41,6 +61,15 @@ export default new Vuex.Store({
       filtered.forEach(task => {
         task.done = !task.done;
       });
+    },
+
+    addLabel(state, { text }) {
+      state.labels.push({
+        id: state.nextLabelId,
+        text
+      });
+
+      state.nextLabelId++;
     }
   },
   actions: {}
